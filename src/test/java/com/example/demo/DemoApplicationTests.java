7148e153772c854e574a914dc8bc4b27ceff324d
lwk <2797841626@qq.com>
2024-03-02 17:40:04 +0800

    item

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.example.demo.Mapper.userMapper;
+import com.example.demo.pojo.User;
+@SpringBootTest
+class DemoApplicationTests {
+
+//	@Autowired
+//	private userMapper userMapper;
+
+	@Test
+	void contextLoads() {
+//		User user = new User();
+//		user.setId(0);
+//		user.setName("lwk");
+//		user.setPassword(123456);
+//		userMapper.insertUser(user);
+	}
+
+}
